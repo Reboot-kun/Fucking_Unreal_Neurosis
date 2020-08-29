0033a14674410259e21ce4912da6feac0b10893b
Reboot-kun <70432237+Reboot-kun@users.noreply.github.com>
2020-08-29 23:12:41 +0900

    help
    help

--- Assets/Dev/Reboot-kun/Scripts/PlayerScript.cs
+++ Assets/Dev/Reboot-kun/Scripts/PlayerScript.cs
@@ -7,6 +7,12 @@ public class PlayerScript : MonoBehaviour
     Rigidbody2D rb2D;
     Vector2 inputDir;
     [SerializeField] float moveSpeed;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashDist;
+    float dashTime;
+    bool isDashing = false;
+    bool canWalk = true;
+    bool canShoot = true;
 
     private void Awake()
     {
@@ -16,10 +22,18 @@ public class PlayerScript : MonoBehaviour
     {
         inputDir.x = GetInput(inputDir.x, KeyCode.D, KeyCode.A);
         inputDir.y = GetInput(inputDir.y, KeyCode.W, KeyCode.S);
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isDashing = true;
+            canWalk = false;
+        }
     }
     private void FixedUpdate()
     {
-        Move();
+        if (canWalk)
+            Walk();
+        if (isDashing)
+            Dash();
     }
 
     float GetInput(float input, KeyCode plusKey, KeyCode minusKey)
@@ -37,11 +51,33 @@ public class PlayerScript : MonoBehaviour
         if (!Input.GetKey(plusKey) && !Input.GetKey(minusKey))
             input = 0;
 
+        if (!canWalk)
+            input = 0;
+
         return input;
     }
 
-    void Move()
+    
+
+    void Walk()
     {
         rb2D.velocity = inputDir.normalized * moveSpeed;
     }
+
+    void Dash()
+    {
+        float dashTimeTotal = dashDist / dashSpeed;
+        Collider2D[] dashHitBox = Physics2D.OverlapCircleAll(transform.position, 0.5f);
+
+        dashTime += Time.deltaTime;
+        if (dashTime >= dashTimeTotal)
+        {
+            isDashing = false;
+            dashTime = 0;
+            canWalk = true;
+            canShoot = true;
+        }
+    }
+
+
 }
