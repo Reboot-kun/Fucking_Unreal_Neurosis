c19296b37beb8efd0145bbadd1efe39bb93505d6
Reboot-kun <70432237+Reboot-kun@users.noreply.github.com>
2020-08-29 20:01:31 +0900

    Dick Destroyer
    420Blazeit

--- Assets/Dev/Reboot-kun/Scripts/PlayerScript.cs
+++ Assets/Dev/Reboot-kun/Scripts/PlayerScript.cs
@@ -3,16 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   int inputDirX;
+    int inputDirY;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        void GetInput(KeyCode plusKeyX, KeyCode minusKeyX, KeyCode plusKeyY, KeyCode minusKeyY)
+        {
+            if (Input.GetKeyDown(plusKeyX))
+                inputDirX = 1;
+            if (Input.GetKeyDown(minusKeyX))
+                inputDirX = -1;
+
+            if (Input.GetKey(minusKeyX) && Input.GetKeyUp(plusKeyX))
+                inputDirX = -1;
+            if (Input.GetKey(plusKeyX) && Input.GetKeyUp(minusKeyX))
+                inputDirX = 1;
+
+            if (!Input.GetKey(plusKeyX) && !Input.GetKey(minusKeyX))
+                inputDirX = 0;
+
+            if (Input.GetKeyDown(plusKeyY))
+                inputDirY = 1;
+            if (Input.GetKeyDown(minusKeyY))
+                inputDirY = -1;
+
+            if (Input.GetKey(minusKeyY) && Input.GetKeyUp(plusKeyY))
+                inputDirY = -1;
+            if (Input.GetKey(plusKeyY) && Input.GetKeyUp(minusKeyY))
+                inputDirY = 1;
+
+            if (!Input.GetKey(plusKeyY) && !Input.GetKey(minusKeyY))
+                inputDirY = 0;
+        }
+        GetInput(KeyCode.D, KeyCode.A, KeyCode.W, KeyCode.S);
+        Debug.Log(inputDirX);
+        Debug.Log(inputDirY);
     }
 }
